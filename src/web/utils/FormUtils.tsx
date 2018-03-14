d00460f6f9a5759ae208816250ed79857ce2dee6
voytech <voytech.m@gmail.com>
2018-03-14 15:38:39 +0100

    Added login screen - content will be moved to MiddleContainer component

---
+++ src/web/utils/FormUtils.tsx
@@ -0,0 +1,5 @@
+import * as  React from 'react';
+import { FormControl } from 'react-bootstrap';
+
+
+export type FormEvent = React.FormEvent<FormControl & HTMLInputElement>
--- src/web/views/user/LoginView.tsx
+++ src/web/views/user/LoginView.tsx
@@ -1,8 +1,43 @@
 import * as  React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { FormGroup, FormControl, ControlLabel, Col  } from 'react-bootstrap';
+import { FormEvent } from '../../utils/FormUtils';
 
-export class LoginView extends React.Component{
+interface IState{
+    userName:string;
+    userPassword:string;
+}
+
+export class LoginView extends React.Component<any,IState>{
+
+  constructor(props){
+    super(props);
+    this.state =  {userName:'',userPassword:''};
+  }
+
+  private setUserName = (e : FormEvent) => {
+    this.setState({userName: e.currentTarget.value})
+  }
+
+  private setUserPassword = (e : FormEvent) => {
+    this.setState({userPassword: e.currentTarget.value})
+  }
+
   render(){
-    return <div>Login</div>;
+    return <div className="row">
+              <Col sm={4} smOffset={4}
+                   md={4} mdOffset={4}
+                   lg={4} lgOffset={4} >
+                <form>
+                 <FormGroup>
+                    <ControlLabel>Enter Login</ControlLabel>
+                    <FormControl type="text" value={this.state.userName} onChange={this.setUserName}></FormControl>
+                 </FormGroup>
+                 <FormGroup>
+                    <ControlLabel>Enter Password</ControlLabel>
+                    <FormControl type="text" value={this.state.userPassword} onChange={this.setUserPassword}></FormControl>
+                 </FormGroup>
+               </form>
+              </Col>
+           </div>
   }
 }
