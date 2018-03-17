b5c78d9c631395bcf4a41879cccce3190b5696a4
voytech <voytech.m@gmail.com>
2018-03-17 22:58:10 +0100

    Adding redux ...

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -9,18 +9,22 @@ import { Main } from './routers/Main'
 import { User } from './routers/User'
 import { Link, Switch, Route, Redirect, HashRouter } from 'react-router-dom';
 import { HeaderComponent } from './components/HeaderComponent'
+import { Provider } from 'react-redux';
+import { store } from './Store'
 //
 class App extends React.Component{
   render(){
-    return <HashRouter>
-             <div className="container-fluid">
-                 <HeaderComponent title='Simple Product Catalogue'></HeaderComponent>
-                 <Switch>
-                   <Route path="/user" name="User" component={User}/>
-                   <Route path="/" name="Home" component={Main}/>
-                 </Switch>
-             </div>
-          </HashRouter>;
+    return <Provider store={store}>
+              <HashRouter>
+                 <div className="container-fluid">
+                     <HeaderComponent title='Simple Product Catalogue'></HeaderComponent>
+                     <Switch>
+                       <Route path="/user" name="User" component={User}/>
+                       <Route path="/" name="Home" component={Main}/>
+                     </Switch>
+                 </div>
+              </HashRouter>
+           </Provider>;
   }
 }
 ReactDOM.render(<App />,document.getElementById("container"));
